700785b7747fee2b8b2da00c3112a2f271ea9c2a
Miroslaw Horbal <miroslaw@gmail.com>
2015-09-29 03:52:39 -0400

    Added the ability to choose the evaluation metric for bandits, and added AnnealedDirichletBandit

--- algorithms.py
+++ algorithms.py
@@ -1,12 +1,12 @@
 from __future__ import division
-from scipy import random, exp, log, argmax, array
+from scipy import random, exp, log, argmax, array, stats
 
 
-TINY = 1e-8
+TINY = 1e-6
 
 
 class BaseBandit(object):
-    def __init__(self, draws=None, payouts=None, success=None, n_arms=None):
+    def __init__(self, draws=None, payouts=None, success=None, n_arms=None, metric='payout'):
         if draws is None or payouts is None or success is None:
             if n_arms is None:
                 raise ValueError('Must give either draws, payouts, and success or n_arms')
@@ -20,10 +20,36 @@ class BaseBandit(object):
                 self.payouts = payouts
                 self.success = success
 
+        self.metric = metric
+
     def initialize(self, n_arms):
         self.draws = [0]*n_arms
         self.payouts = [0]*n_arms
         self.success = [0]*n_arms
+
+    @property
+    def metric(self):
+        return self._metric
+
+    @metric.setter
+    def metric(self, new_metric):
+        if new_metric in {'Epayout', 'Esuccess', 'payout', 'success'}:
+            self._metric = new_metric
+        else:
+            raise ValueError('metric must be either "payout", "success", "Epayout", or "Esuccess"')
+
+    def _metric_fn(self):
+        if self.metric == 'payout':
+            return self.payouts
+
+        elif self.metric == 'success':
+            return self.success
+
+        elif self.metric == 'Epayout':
+            return self.expected_payouts
+
+        elif self.metric == 'Esuccess':
+            return self.expected_success
 
     @property
     def total_draws(self):
@@ -49,10 +75,10 @@ class BaseBandit(object):
     def expected_payouts(self):
         return [p/d if d > 0 else 0 for p, d in zip(self.payouts, self.draws)]
 
-    def update(self, selected_arm, reward):
+    def update(self, selected_arm, payout):
         self.draws[selected_arm] += 1
-        self.payouts[selected_arm] += reward
-        self.success[selected_arm] += 1 if reward > 0 else 0
+        self.payouts[selected_arm] += payout
+        self.success[selected_arm] += 1 if payout > 0 else 0
 
     def draw(self):
         raise NotImplementedError('This is a baseclass, inherit this class and implement a "draw" method')
@@ -67,7 +93,7 @@ def logarithmic_schedule(t):
 
 
 class AnnealedBaseBandit(BaseBandit):
-    def __init__(self, schedule='linear', **kwargs):
+    def __init__(self, schedule='logarithmic', **kwargs):
         self.schedule = schedule
         super(AnnealedBaseBandit, self).__init__(**kwargs)
 
@@ -101,7 +127,7 @@ class EpsilonGreedyBandit(BaseBandit):
         if random.rand() < self.epsilon:
             return random.choice(self.n_arms)
         else:
-            return argmax(self.expected_payouts)
+            return argmax(self._metric_fn())
 
 
 class AnnealedEpsilonGreedyBandit(AnnealedBaseBandit):
@@ -114,7 +140,7 @@ class AnnealedEpsilonGreedyBandit(AnnealedBaseBandit):
         if random.rand() < self.epsilon * temp:
             return random.choice(self.n_arms)
         else:
-            return argmax(self.expected_payouts)
+            return argmax(self._metric_fn())
 
 
 def softmax(l):
@@ -124,13 +150,13 @@ def softmax(l):
 
 class SoftmaxBandit(BaseBandit):
     def draw(self):
-        return argmax(random.multinomial(1, pvals=softmax(self.expected_payouts)))
+        return argmax(random.multinomial(1, pvals=softmax(self._metric_fn())))
 
 
 class AnnealedSoftmaxBandit(AnnealedBaseBandit):
     def draw(self):
         temp = 1 / self._schedule_fn(self.total_draws)
-        return argmax(random.multinomial(1, pvals=softmax(array(self.expected_payouts) / temp)))
+        return argmax(random.multinomial(1, pvals=softmax(array(self._metric_fn()) / temp)))
 
 
 class DirichletBandit(BaseBandit):
@@ -139,7 +165,7 @@ class DirichletBandit(BaseBandit):
         super(DirichletBandit, self).__init__(**kwargs)
 
     def draw(self):
-        x = array(self.expected_payouts) + 1
+        x = array(self._metric_fn()) + 1
 
         if self.random_sample:
             pvals = random.dirichlet(x)
@@ -147,3 +173,22 @@ class DirichletBandit(BaseBandit):
             pvals = x / sum(x)
 
         return argmax(random.multinomial(1, pvals=pvals))
+
+
+class AnnealedDirichletBandit(AnnealedBaseBandit):
+    def __init__(self, random_sample=True, **kwargs):
+        self.random_sample = random_sample
+        super(AnnealedDirichletBandit, self).__init__(**kwargs)
+
+    def draw(self):
+        temp = 1 / self._schedule_fn(self.total_draws)
+        x = array(self._metric_fn()) / temp + 1
+
+        if self.random_sample:
+            pvals = random.dirichlet(x)
+        else:
+            pvals = x / sum(x)
+
+        return argmax(random.multinomial(1, pvals=pvals))
+
+
